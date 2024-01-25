0cf39e902714e2fc36181c76236d02de668bde69
Zena <33304871+Heojiyeon@users.noreply.github.com>
2024-01-25 09:46:05 +0900

    refactor: 문제 데이터 fetch 함수 제거 및 query 결과 값 적용

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -2,8 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-import { addIndexedDB, getIndexedDB } from '@/data';
-import { GameResult, Games } from '@/types/problem';
+import { addIndexedDB } from '@/data';
+import { Games } from '@/types/problem';
 
 const Iframe = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -18,69 +18,6 @@ const Iframe = () => {
     enabled: isLoaded && gameType ? true : false,
     refetchOnMount: false,
   });
-
-  const fetchData = async (gameType: Games) => {
-    return await getIndexedDB({ gameType });
-  };
-
-  useEffect(() => {
-    const handleMessage = async (e: MessageEvent) => {
-      e.preventDefault();
-
-      // 소요 시간 계산
-      const calculateElapsedTime = () => {
-        const endTime = new Date().getTime();
-        const startTime = window.localStorage.getItem('gameStartTime');
-        let currentElapsedTime: number;
-
-        if (startTime !== null) {
-          currentElapsedTime = (endTime - Number(startTime)) / 1000;
-          return currentElapsedTime;
-        }
-      };
-      const elapsedTime = calculateElapsedTime()!;
-
-      // 게임 유형에 따라 결과를 저장하는 함수
-      const addResultByGameType = async (gameType: Games) => {
-        // 가져와서 length 체크
-        const checkResultLength = async () => {
-          const currentLength = (await fetchData(gameType)) as GameResult[];
-          return currentLength.length;
-        };
-
-        const currentLength = await checkResultLength();
-
-        // length에 따라 order 부여 후 DB에 add
-        const currentResult = {
-          order: currentLength as unknown as number,
-          count: e.data[0],
-          elapsedTime: elapsedTime.toFixed(2) as unknown as string,
-        };
-
-        addIndexedDB({ gameType: gameType, result: currentResult });
-        window.location.replace('/result');
-      };
-
-      switch (gameType) {
-        case 'number-game':
-          addResultByGameType('number-game');
-          break;
-        case 'situation-game':
-          addResultByGameType('situation-game');
-          break;
-
-        default:
-          break;
-      }
-    };
-
-    window.addEventListener('message', handleMessage);
-
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
-  }, [gameType]);
-
   const getProblem = async (gameType: Games) => {
     const response = await axios.get(`/problem/${gameType}`);
     return response.data;
@@ -93,8 +30,59 @@ const Iframe = () => {
         document.querySelector('iframe');
 
       $iframe?.contentWindow?.postMessage(data);
+
+      // 이벤트 리스너 등록
+      const handleMessage = async (e: MessageEvent) => {
+        e.preventDefault();
+
+        // 소요 시간 계산
+        const calculateElapsedTime = () => {
+          const endTime = new Date().getTime();
+          const startTime = window.localStorage.getItem('gameStartTime');
+          let currentElapsedTime: number;
+
+          if (startTime !== null) {
+            currentElapsedTime = (endTime - Number(startTime)) / 1000;
+            return currentElapsedTime;
+          }
+        };
+        const elapsedTime = calculateElapsedTime()!;
+
+        // 게임 유형에 따라 결과를 저장하는 함수
+        const addResultByGameType = async (gameType: Games) => {
+          const currentLength = data.length;
+
+          // length에 따라 order 부여 후 DB에 add
+          const currentResult = {
+            order: currentLength as unknown as number,
+            count: e.data[0],
+            elapsedTime: elapsedTime.toFixed(2) as unknown as string,
+          };
+
+          addIndexedDB({ gameType: gameType, result: currentResult });
+          window.location.replace('/result');
+        };
+
+        switch (gameType) {
+          case 'number-game':
+            addResultByGameType('number-game');
+            break;
+          case 'situation-game':
+            addResultByGameType('situation-game');
+            break;
+
+          default:
+            break;
+        }
+      };
+
+      window.addEventListener('message', handleMessage);
+
+      return () => {
+        window.removeEventListener('message', handleMessage);
+      };
     }
-  }, [data]);
+  }, [data, gameType]);
 
   useEffect(() => {
     const currentGameType = window.localStorage.getItem('gameType');
