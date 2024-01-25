b82b2bce75c8ded432775ac1cdc7f662bf89a9d1
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2024-01-26 00:32:56 +0900

    feat: 404 페이지 구현

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@/components/common/Button';
+
 const NotFoundPage = () => {
-  return <div>not found page</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center mt-60">
+      <img src="/images/no-result.png" alt="포이 이미지" />
+      <div className="text-[24px] text-[#A5A5A5] mt-8">잘못된 페이지에요..</div>
+      <Button
+        id="login"
+        content="처음으로 돌아가기"
+        size="w-64 h-10 mt-16"
+        onClick={() => navigate('/')}
+      />
+    </div>
+  );
 };
 
 export default NotFoundPage;
